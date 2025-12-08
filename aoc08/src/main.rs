e394c059ec2e36a54ceb937808bb37e1b5f7155f
Stephen Hines <srhines@gmail.com>
2025-12-08 02:33:11 -0800

    Cleanup of Day 8 code

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -15,6 +15,53 @@ fn get_input(filename: &str) -> Vec<String> {
     lines
 }
 
+struct CircuitSet {
+    circuits: Vec<HashSet<usize>>,
+}
+
+impl CircuitSet {
+    fn create(n: usize) -> Self {
+        let mut circuits = Vec::new();
+        for i in 0..n {
+            let mut s: HashSet<usize> = HashSet::new();
+            s.insert(i);
+            circuits.push(s);
+        }
+
+        CircuitSet { circuits }
+    }
+
+    fn connect_boxes(&mut self, a: usize, b: usize) {
+        let mut aset = (false, 0);
+        let mut bset = (false, 0);
+        for (ci, c) in self.circuits.iter().enumerate() {
+            if c.contains(&a) {
+                aset = (true, ci);
+            }
+            if c.contains(&b) {
+                bset = (true, ci);
+            }
+            if aset.0 && bset.0 {
+                if aset.1 == bset.1 {
+                    // They're already connected
+                } else {
+                    // Not the most efficient use of HashSet union/extend, but it works
+                    let mut new_set = self.circuits[aset.1].clone();
+                    new_set.extend(&self.circuits[bset.1]);
+                    self.circuits[aset.1] = new_set;
+                    self.circuits.remove(bset.1);
+                    //println!("circuits[{}]: {:?}", self.circuits.len(), self.circuits);
+                }
+                break;
+            }
+        }
+    }
+
+    fn sort(&mut self) {
+        self.circuits.sort_by(|a, b| b.len().cmp(&a.len()));
+    }
+}
+
 struct JunctionBox {
     x: i64,
     y: i64,
@@ -28,179 +75,111 @@ impl JunctionBox {
     }
 }
 
-fn read_junctions(lines: &Vec<String>) -> Vec<JunctionBox> {
-    let mut boxes = Vec::new();
-
-    for line in lines {
-        let coords = line.split(',').collect::<Vec<_>>();
-        assert_eq!(coords.len(), 3);
-        let (x, y, z) = (
-            coords[0].parse::<i64>().unwrap(),
-            coords[1].parse::<i64>().unwrap(),
-            coords[2].parse::<i64>().unwrap(),
-        );
-        boxes.push(JunctionBox { x, y, z });
-    }
-
-    boxes
+struct BoxGrid {
+    boxes: Vec<JunctionBox>,
+    distances: Vec<(f32, usize, usize)>,
 }
 
-fn compute_circuits(boxes: &Vec<JunctionBox>, connections: usize, num_circuits: usize) -> usize {
-    let mut product = 1;
-    assert!(connections > 0);
-    assert!(num_circuits > 0);
+impl BoxGrid {
+    fn create(lines: &Vec<String>) -> Self {
+        let mut boxes = Vec::new();
 
-    let entries = boxes.len();
-    let mut distances = Vec::new();
-    for i in 0..entries {
-        for j in i + 1..entries {
-            let b = &boxes[i];
-            let b2 = &boxes[j];
-            distances.push((b.dist(b2), i, j));
+        for line in lines {
+            let coords = line.split(',').collect::<Vec<_>>();
+            assert_eq!(coords.len(), 3);
+            let (x, y, z) = (
+                coords[0].parse::<i64>().unwrap(),
+                coords[1].parse::<i64>().unwrap(),
+                coords[2].parse::<i64>().unwrap(),
+            );
+            boxes.push(JunctionBox { x, y, z });
         }
-    }
-    distances.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
-    let mut circuits = Vec::new();
-    for i in 0..entries {
-        let mut s: HashSet<usize> = HashSet::new();
-        s.insert(i);
-        circuits.push(s);
-    }
-
-    // Use n to walk throught the sorted (least to greatest) distances
-    let mut n = 0;
-    while n < connections {
-        let (_, a, b) = distances[n];
-        let mut aset = (false, 0);
-        let mut bset = (false, 0);
-        for (ci, c) in circuits.iter().enumerate() {
-            if c.contains(&a) {
-                aset = (true, ci);
-            }
-            if c.contains(&b) {
-                bset = (true, ci);
-            }
-            if aset.0 && bset.0 {
-                if aset.1 == bset.1 {
-                    // They're already connected
-                } else {
-                    // Not the most efficient use of HashSet union/extend, but it works
-                    let mut new_set = circuits[aset.1].clone();
-                    new_set.extend(&circuits[bset.1]);
-                    circuits[aset.1] = new_set;
-                    circuits.remove(bset.1);
-                    //println!("circuits[{}]: {circuits:?}", circuits.len());
-                }
-                break;
+        let entries = boxes.len();
+        let mut distances = Vec::new();
+        for i in 0..entries {
+            for j in i + 1..entries {
+                let b = &boxes[i];
+                let b2 = &boxes[j];
+                distances.push((b.dist(b2), i, j));
             }
         }
-        n += 1;
+        distances.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        BoxGrid { boxes, distances }
     }
 
-    circuits.sort_by(|a, b| b.len().cmp(&a.len()));
-    //println!("sorted circuits[{}]: {circuits:?}", circuits.len());
+    fn circuit_trio_product(&self, connections: usize) -> usize {
+        assert!(connections > 0);
 
-    for i in 0..num_circuits {
-        product *= circuits[i].len();
-    }
+        let mut circuitset = CircuitSet::create(self.boxes.len());
 
-    println!("product: {product}");
-    product
-}
-
-fn compute_circuits_complete(boxes: &Vec<JunctionBox>) -> i64 {
-    let entries = boxes.len();
-    let mut distances = Vec::new();
-    for i in 0..entries {
-        for j in i + 1..entries {
-            let b = &boxes[i];
-            let b2 = &boxes[j];
-            distances.push((b.dist(b2), i, j));
+        for n in 0..connections {
+            let (_, a, b) = self.distances[n];
+            circuitset.connect_boxes(a, b);
         }
-    }
-    distances.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
-    let mut circuits = Vec::new();
-    for i in 0..entries {
-        let mut s: HashSet<usize> = HashSet::new();
-        s.insert(i);
-        circuits.push(s);
+        circuitset.sort();
+
+        // Product of the trio of largest circuits
+        assert!(circuitset.circuits.len() >= 3);
+        let mut product = 1;
+        for i in 0..3 {
+            product *= circuitset.circuits[i].len();
+        }
+
+        println!("product: {product}");
+        product
     }
 
-    // Use n to walk throught the sorted (least to greatest) distances
-    let mut n = 0;
-    let connections = distances.len();
-    while n < connections {
-        let (_, a, b) = distances[n];
-        let mut aset = (false, 0);
-        let mut bset = (false, 0);
-        for (ci, c) in circuits.iter().enumerate() {
-            if c.contains(&a) {
-                aset = (true, ci);
-            }
-            if c.contains(&b) {
-                bset = (true, ci);
-            }
-            if aset.0 && bset.0 {
-                if aset.1 == bset.1 {
-                    // They're already connected
-                } else {
-                    // Not the most efficient use of HashSet union/extend, but it works
-                    let mut new_set = circuits[aset.1].clone();
-                    new_set.extend(&circuits[bset.1]);
-                    circuits[aset.1] = new_set;
-                    circuits.remove(bset.1);
-                    //println!("circuits[{}]: {circuits:?}", circuits.len());
-                    if circuits.len() == 1 {
-                        let b1 = &boxes[a];
-                        let b2 = &boxes[b];
-                        let product = b1.x * b2.x;
-                        println!("product (last 2 x): {product}");
-                        return product;
-                    }
-                }
-                break;
+    fn circuit_last_x_product(&self) -> i64 {
+        let mut circuitset = CircuitSet::create(self.boxes.len());
+
+        for (_, a, b) in &self.distances {
+            circuitset.connect_boxes(*a, *b);
+            if circuitset.circuits.len() == 1 {
+                let product = &self.boxes[*a].x * &self.boxes[*b].x;
+                println!("product (last 2 x): {product}");
+                return product;
             }
         }
-        n += 1;
+        unreachable!("Failed to converge all junction boxes");
     }
-    panic!("Failed to converge all junction boxes");
 }
 
 #[test]
 fn test_prelim() {
-    let boxes = read_junctions(&get_input("prelim.txt"));
-    let product = compute_circuits(&boxes, 10, 3);
+    let boxgrid = BoxGrid::create(&get_input("prelim.txt"));
+    let product = boxgrid.circuit_trio_product(10);
     assert_eq!(product, 40);
 }
 
 #[test]
 fn test_part1() {
-    let boxes = read_junctions(&get_input("input.txt"));
-    let product = compute_circuits(&boxes, 1000, 3);
+    let boxgrid = BoxGrid::create(&get_input("input.txt"));
+    let product = boxgrid.circuit_trio_product(1000);
     assert_eq!(product, 81536);
 }
 
 #[test]
 fn test_prelim2() {
-    let boxes = read_junctions(&get_input("prelim.txt"));
-    let product = compute_circuits_complete(&boxes);
+    let boxgrid = BoxGrid::create(&get_input("prelim.txt"));
+    let product = boxgrid.circuit_last_x_product();
     assert_eq!(product, 25272);
 }
 
 #[test]
 fn test_part2() {
-    let boxes = read_junctions(&get_input("input.txt"));
-    let product = compute_circuits_complete(&boxes);
+    let boxgrid = BoxGrid::create(&get_input("input.txt"));
+    let product = boxgrid.circuit_last_x_product();
     assert_eq!(product, 7017750530);
 }
 
 fn main() {
-    let boxes = read_junctions(&get_input("prelim.txt"));
-    compute_circuits(&boxes, 10, 3);
-    compute_circuits_complete(&boxes);
-    let boxes = read_junctions(&get_input("input.txt"));
-    compute_circuits(&boxes, 1000, 3);
-    compute_circuits_complete(&boxes);
+    let boxgrid = BoxGrid::create(&get_input("prelim.txt"));
+    boxgrid.circuit_trio_product(10);
+    boxgrid.circuit_last_x_product();
+    let boxgrid = BoxGrid::create(&get_input("input.txt"));
+    boxgrid.circuit_trio_product(10);
+    boxgrid.circuit_last_x_product();
 }
